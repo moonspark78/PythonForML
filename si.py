3c036de1f33173994b8c5f7bff8fdc82f0bb5316
Omar <jabour.omar@hotmail.com>
2025-10-30 23:15:44 +0100

    :art: test with if    :art:

--- si.py
+++ si.py
@@ -1,3 +1,17 @@
-x = 1
+x = 0
 if x > 0:
     print(x, "Positive")
+else:
+    print(x, "Negative")
+
+
+def test():
+    if x > 0:
+        print(x, "Positive")
+    elif x == 0:
+        print(x, "Zero")
+    else:
+        print(x, "Negative")
+
+
+test()
